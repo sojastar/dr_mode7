2922b81d99108be161ca50b6a02bc5a397e7b9b6
Julien Jassaud <julienjassaud@Mac-mini-de-Julien.local>
2020-06-11 15:31:33 +0200

    Adding textures and tilemaps for testing.

--- app/main.rb
+++ app/main.rb
@@ -1,17 +1,47 @@
-require 'lib/mode7.rb'
+#require 'lib/mode7.rb'
 
 
 def setup(args)
-
-
+  #args.render_target(:road).width   = 91
+  #args.render_target(:road).height  = 91
 
   args.state.setup_done = true
 end
 
 
 def tick(args)
-  setup(args) unless setup_done
+  setup(args) unless args.state.setup_done
 
+  angle = ( args.state.tick_count % 720 ) / 2.0
 
+  args.render_target(:road).sprites << {  x:      16,
+                                          y:      16,
+                                          w:      64,
+                                          h:      64,
+                                          path:   'sprites/road_test.png',
+                                          angle:  angle }
+
+  args.outputs.sprites << { x:    200,
+                            y:    200,
+                            w:    1280,
+                            h:    720,
+                            path: :road }
+                            #tile_x: 0,
+                            #tile_y: -675,
+                            #tile_w: 90,
+                            #tile_h: 1 }
+
+  args.outputs.sprites << { x:      400,
+                            y:      216,
+                            w:      128,
+                            h:      2,
+                            path:   :road,
+                            tile_x: 0,
+                            tile_y: 675,
+                            tile_w: 90,
+                            tile_h: 1 }
+  
+  args.outputs.lines << [ 200, 200, 200, 500, 0, 0, 0, 25 ]
+  args.outputs.lines << [ 200, 200, 500, 200, 0, 0, 0, 25 ]
 
 end
---
+++ sprites/test track tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="test track tiles" tilewidth="8" tileheight="8" tilecount="16" columns="4">
+ <image source="tiles.png" width="32" height="32"/>
+</tileset>
