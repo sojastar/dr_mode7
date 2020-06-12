f3e7c5c0fdb8b44e8a77ccfad5bf70d4eff1af83
Julien Jassaud <sojastar07@gmail.com>
2020-06-12 10:42:36 +0200

    Further experimenting with roations.

--- app/main.rb
+++ app/main.rb
@@ -2,46 +2,80 @@
 
 
 def setup(args)
-  #args.render_target(:road).width   = 91
-  #args.render_target(:road).height  = 91
+  args.state.player.x         = 480 - 72 
+  args.state.player.y         = 128
+  args.state.player.direction = 0.0
+  args.state.player.dx        = 5 * Math::cos( deg_to_rad(args.state.player.direction) + Math::PI/2.0 )
+  args.state.player.dy        = 5 * Math::sin( deg_to_rad(args.state.player.direction) + Math::PI/2.0 )
+  #args.state.player.dx        = 5 * Math::cos( deg_to_rad(args.state.player.direction) )
+  #args.state.player.dy        = 5 * Math::sin( deg_to_rad(args.state.player.direction) )
 
   args.state.setup_done = true
+
+  puts 'setup done'
 end
 
 
 def tick(args)
   setup(args) unless args.state.setup_done
 
-  angle = ( args.state.tick_count % 720 ) / 2.0
 
-  args.render_target(:road).sprites << {  x:      16,
-                                          y:      16,
-                                          w:      64,
-                                          h:      64,
-                                          path:   'sprites/road_test.png',
-                                          angle:  angle }
+  if    args.inputs.keyboard.key_held.left then
+    args.state.player.direction  -= 0.5
+    args.state.player.dx          = 5 * Math::cos( deg_to_rad(args.state.player.direction) + Math::PI/2.0 )
+    args.state.player.dy          = 5 * Math::sin( deg_to_rad(args.state.player.direction) + Math::PI/2.0 )
+    #args.state.player.dx          = 5 * Math::cos( deg_to_rad(args.state.player.direction) )
+    #args.state.player.dy          = 5 * Math::sin( deg_to_rad(args.state.player.direction) )
+  elsif args.inputs.keyboard.key_held.right then
+    args.state.player.direction  += 0.5
+    args.state.player.dx          = 5 * Math::cos( deg_to_rad(args.state.player.direction) + Math::PI/2.0 )
+    args.state.player.dy          = 5 * Math::sin( deg_to_rad(args.state.player.direction) + Math::PI/2.0 )
+    #args.state.player.dx          = 5 * Math::cos( deg_to_rad(args.state.player.direction) )
+    #args.state.player.dy          = 5 * Math::sin( deg_to_rad(args.state.player.direction) )
+  end
 
-  args.outputs.sprites << { x:    200,
-                            y:    200,
-                            w:    1280,
-                            h:    720,
-                            path: :road }
-                            #tile_x: 0,
-                            #tile_y: -675,
-                            #tile_w: 90,
-                            #tile_h: 1 }
+  if args.inputs.keyboard.key_held.up then
+    args.state.player.x          += args.state.player.dx
+    args.state.player.y          += args.state.player.dy
+  end
 
-  args.outputs.sprites << { x:      400,
-                            y:      216,
-                            w:      128,
-                            h:      2,
-                            path:   :road,
-                            tile_x: 0,
-                            tile_y: 675,
-                            tile_w: 90,
-                            tile_h: 1 }
+
+  args.render_target(:road).sprites << {  x:      340 - args.state.player.x,
+                                          y:      340 - args.state.player.y,
+                                          w:      480,
+                                          h:      480,
+                                          path:   'sprites/track_test.png',
+                                          angle:  args.state.player.direction,
+                                          angle_anchor_x: args.state.player.x / 480.0,
+                                          angle_anchor_y: args.state.player.y / 480.0 }
+
+  450.times do |y|
+    args.outputs.sprites << { x:      0,
+                              y:      y,
+                              w:      680,
+                              h:      1,
+                              path:   :road,
+                              tile_x: 0,
+                              tile_y: 720 - y,
+                              tile_w: 680,
+                              tile_h: 1 }
+  end
   
-  args.outputs.lines << [ 200, 200, 200, 500, 0, 0, 0, 25 ]
-  args.outputs.lines << [ 200, 200, 500, 200, 0, 0, 0, 25 ]
+  args.outputs.lines << [ 320, 320, 360, 360, 0, 0, 0, 255 ]
+  args.outputs.lines << [ 320, 360, 360, 320, 0, 0, 0, 255 ]
+  #args.outputs.lines << [ 340,
+  #                        340,
+  #                        340 + 10 * args.state.player.dx,
+  #                        340 + 10 * args.state.player.dy,
+  #                        255, 0, 0, 255 ]
 
+end
+
+
+def deg_to_rad(angle)
+  angle * Math::PI / 180.0
+end
+
+def rad_to_deg(angle)
+  angle * 180.0 / Math::PI
 end
