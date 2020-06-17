86ecae1b7bdac9ac9e5798ba868fe2db27b38c7d
Julien Jassaud <julienjassaud@Mac-mini-de-Julien.local>
2020-06-17 17:10:52 +0200

    Code clean-up, mostly replacing magic numbers with properly named constants.

--- app/main.rb
+++ app/main.rb
@@ -5,13 +5,24 @@
 
 
 # ---=== CONSTANTS : ===---
-#PIXEL_SCALE         = 8
-#SCREEN_HALF_WIDTH   = ( 1280 / PIXEL_SCALE ) / 2
-RASTER_HEIGHT       = 80
-RASTER_SCAN_MAX     = 24 
-RASTER_SCAN_MIN     = 1 
+SCREEN_WIDTH          = 1280
+SCREEN_HALF_WIDTH     = 640
+SCREEN_HEIGHT         = 720
+SCREEN_HALF_HEIGHT    = 360
 
-ROTATION_SPEED      = 2.0
+PIXEL_SCALE           = 8
+RASTER_HEIGHT         = 80
+RASTER_SCAN_MAX       = 24 
+RASTER_SCAN_MIN       = 1
+
+ROAD_SIZE             = 636
+ROTATED_ROAD_MAX_SIZE = 900
+
+CAMERA_OFFSET         = 48
+FOCAL                 = 80
+
+TRANSLATION_SPEED     = 3
+ROTATION_SPEED        = 2.0
 
 
 
@@ -19,8 +30,8 @@ ROTATION_SPEED      = 2.0
 
 # ---=== SETUP : ===---
 def setup(args)
-  args.state.player.x         = 636 - 176#480 - 72 
-  args.state.player.y         = 192#128
+  args.state.player.x         = ROAD_SIZE - 176 
+  args.state.player.y         = 192
   args.state.player.direction = 0.0
   args.state.player.dx        = 5 * Math::cos( args.state.player.direction.to_radians + Math::PI/2.0 )
   args.state.player.dy        = 5 * Math::sin( args.state.player.direction.to_radians + Math::PI/2.0 )
@@ -28,16 +39,13 @@ def setup(args)
   args.state.raster_height    = RASTER_HEIGHT
   args.state.raster_scan_min  = RASTER_SCAN_MIN
   args.state.raster_scan_max  = RASTER_SCAN_MAX
-  #args.state.slope            = ( RASTER_SCAN_MIN - RASTER_SCAN_MAX ) / RASTER_HEIGHT
   args.state.slope            = ( args.state.raster_scan_min - args.state.raster_scan_max ) / args.state.raster_height
   args.state.intercept        = RASTER_SCAN_MAX
 
-  args.state.focal            = 80
+  args.state.focal            = FOCAL
   args.state.height           = RASTER_HEIGHT
 
   args.state.setup_done       = true
-
-  puts 'setup done'
 end
 
 
@@ -46,21 +54,21 @@ end
 
 # ---=== MAIN LOOP : ===---
 def tick(args)
-  # --- Setup :
+  # --- 1. Setup :
   setup(args) unless args.state.setup_done
 
 
-  # --- User Inputs :
+  # --- 2. User Inputs :
  
-  # Player control :
+  # - 2.1 Player control :
   if    args.inputs.keyboard.key_held.left then
     args.state.player.direction  -= ROTATION_SPEED
-    args.state.player.dx          = 5 * Math::cos( args.state.player.direction.to_radians + Math::PI/2.0 )
-    args.state.player.dy          = 5 * Math::sin( args.state.player.direction.to_radians + Math::PI/2.0 )
+    args.state.player.dx          = TRANSLATION_SPEED * Math::cos( args.state.player.direction.to_radians + Math::PI/2.0 )
+    args.state.player.dy          = TRANSLATION_SPEED * Math::sin( args.state.player.direction.to_radians + Math::PI/2.0 )
   elsif args.inputs.keyboard.key_held.right then
     args.state.player.direction  += ROTATION_SPEED
-    args.state.player.dx          = 5 * Math::cos( args.state.player.direction.to_radians + Math::PI/2.0 )
-    args.state.player.dy          = 5 * Math::sin( args.state.player.direction.to_radians + Math::PI/2.0 )
+    args.state.player.dx          = TRANSLATION_SPEED * Math::cos( args.state.player.direction.to_radians + Math::PI/2.0 )
+    args.state.player.dy          = TRANSLATION_SPEED * Math::sin( args.state.player.direction.to_radians + Math::PI/2.0 )
   end
 
   if args.inputs.keyboard.key_held.up then
@@ -68,7 +76,7 @@ def tick(args)
     args.state.player.y          += args.state.player.dy
   end
 
-  # Geometry control :
+  # -2.2 Geometry control :
   if args.inputs.keyboard.key_down.one then
     args.state.raster_scan_min -= 2 
     args.state.slope            = ( args.state.raster_scan_min - args.state.raster_scan_max ) / args.state.raster_height
@@ -97,15 +105,15 @@ def tick(args)
 
   end
 
-  # --- Rasterizing :
-  args.render_target(:road).sprites << {  x:              450 - args.state.player.x,
-                                          y:              450 - args.state.player.y,
-                                          w:              636,
-                                          h:              636,
+  # --- 3. Rasterizing :
+  args.render_target(:road).sprites << {  x:              ( ROTATED_ROAD_MAX_SIZE >> 1 ) - args.state.player.x,
+                                          y:              ( ROTATED_ROAD_MAX_SIZE >> 1 ) - args.state.player.y,
+                                          w:              ROAD_SIZE,
+                                          h:              ROAD_SIZE,
                                           path:           'sprites/track_test.png',
                                           angle:          args.state.player.direction,
-                                          angle_anchor_x: args.state.player.x / 636.0,
-                                          angle_anchor_y: ( args.state.player.y + 48 ) / 636.0 }
+                                          angle_anchor_x: args.state.player.x / ROAD_SIZE,
+                                          angle_anchor_y: ( args.state.player.y + CAMERA_OFFSET ) / ROAD_SIZE }
 
   # DEBUG DEBUG DEBUG !!!
   #draw_cross( args.render_target(:road).lines,
@@ -119,23 +127,23 @@ def tick(args)
   #                          path: :road }
 
 
-  0.upto(RASTER_HEIGHT) do |y|
+  0.upto(args.state.raster_height) do |y|
     distance    = args.state.focal * args.state.raster_height / ( args.state.raster_height + 1 - y )
     scale       = args.state.slope * y + args.state.intercept
-    args.outputs.sprites << { x:      640 - scale * 450 - ( ( scale * 450 ) % 8 ),
-                              y:      8 * y,
-                              w:      scale * 900,
-                              h:      8,
+    args.outputs.sprites << { x:      SCREEN_HALF_WIDTH - scale * ( ROTATED_ROAD_MAX_SIZE >> 1 ),
+                              y:      PIXEL_SCALE * y,
+                              w:      scale * ROTATED_ROAD_MAX_SIZE,
+                              h:      PIXEL_SCALE,
                               path:   :road,
                               tile_x: 0,
-                              tile_y: 720 - 450 - distance,
-                              tile_w: 900,
+                              tile_y: 720 - ( ROTATED_ROAD_MAX_SIZE >> 1 ) - distance,
+                              tile_w: ROTATED_ROAD_MAX_SIZE,
                               tile_h: 1 }
   end
  
 
-  # --- Background :
-  args.outputs.solids << [ 0, 0, 1279, 719, 100, 150, 255, 255 ]
+  # --- 4. Background :
+  args.outputs.solids << [ 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 100, 150, 255, 255 ]
 end
 
 def draw_cross(dest,x,y,color)
