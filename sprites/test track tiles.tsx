ab69e4d2c046e0fff73cc8c39453b1faa4915441
Julien Jassaud <julienjassaud@Mac-mini-de-Julien.local>
2020-06-19 12:11:12 +0200

    Started creating new assets to prepare for tilling implementation.

--- sprites/test track tiles.tsx
+++ sprites/test track tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="test track tiles" tilewidth="8" tileheight="8" tilecount="16" columns="4">
- <image source="tiles.png" width="32" height="32"/>
+<tileset version="1.2" tiledversion="1.3.5" name="test track tiles" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="tiles.png" width="128" height="128"/>
 </tileset>
